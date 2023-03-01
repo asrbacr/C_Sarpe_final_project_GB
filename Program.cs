ca5478d85abc6d7d32db3e58b5eedae32280df60
Vasiliy Gorulev <89159793029@ya.ru>
2023-03-01 20:22:16 +0300

    Добавлено тело алгоритма по задаче 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите массив целых чисел через пробел:");
+string temp = Console.ReadLine()!;
+string[] array = temp.Split(" ");
+
+
+int newArrayLight = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+        newArrayLight++;
+}
+
+string[] newArray = new string[newArrayLight];
+
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[j] = array[i];
+        j++;
+    }
+}
+
+void PrintArray(string[] array1)
+{
+    System.Console.Write($"[{String.Join(",", array1)}]");
+}
+
+PrintArray(newArray);
