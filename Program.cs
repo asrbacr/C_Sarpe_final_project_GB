4972c35d0685b57334cf34fd89a71ce4a5382b35
Vasiliy Gorulev <89159793029@ya.ru>
2023-03-01 20:39:18 +0300

    Поместил задачу 4 в функцию, украсил код

--- Program.cs
+++ Program.cs
@@ -1,31 +1,42 @@
-﻿Console.WriteLine("Введите массив целых чисел через пробел:");
+﻿Console.WriteLine("Введите строчные данные с которыми будет работать программа, отделяя элементы пробелами:");
 string temp = Console.ReadLine()!;
 string[] array = temp.Split(" ");
 
+System.Console.WriteLine();
+System.Console.Write("Получился массив: ");
+PrintArray(array);
+System.Console.WriteLine();
+Console.WriteLine("Программа отобрала и сформировала новый массив на условии, что длина элемента меньше либо равно 3:");
+PrintArray(SerchMalZnach(array));
 
-int newArrayLight = 0;
+//__func
 
-for (int i = 0; i < array.Length; i++)
+string[] SerchMalZnach(string[] array)
 {
-    if (array[i].Length <= 3)
-        newArrayLight++;
-}
+    int newArrayLight = 0;
 
-string[] newArray = new string[newArrayLight];
-
-int j = 0;
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i].Length <= 3)
+    for (int i = 0; i < array.Length; i++)
     {
-        newArray[j] = array[i];
-        j++;
+        if (array[i].Length <= 3)
+            newArrayLight++;
     }
+
+    string[] newArray = new string[newArrayLight];
+
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
 }
 
 void PrintArray(string[] array1)
 {
-    System.Console.Write($"[{String.Join(",", array1)}]");
+    System.Console.Write($"[{String.Join(", ", array1)}]");
 }
 
-PrintArray(newArray);
